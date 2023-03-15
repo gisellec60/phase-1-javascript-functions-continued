ca20872e4ec941abd78f1bbd3b87426248b4004d
Giselle Smith <gisellec60@gmail.com>
2023-03-15 15:18:54 -0400

    more functions completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+    
+}
+
+const mondayWork = function(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+const wrapAdjective = function (visualFlair="*") {
+    return function (adj = "special") {
+        return `You are ${visualFlair}${adj}${visualFlair}!`;
+    }
+} 
